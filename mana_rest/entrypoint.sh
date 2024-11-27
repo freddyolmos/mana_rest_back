2b4143a95729623f452f2738c27c69f03f3cbeec
freddyolmos <fidelolmos334@gmail.com>
2024-11-27 12:47:38 -0600

    Delete migrations 15 and 16

--- mana_rest/entrypoint.sh
+++ mana_rest/entrypoint.sh
@@ -6,6 +6,18 @@ until nc -z -v -w30 db 3306; do
   sleep 1
 done
 
+# Ejecutar makemigrations
+echo "Ejecutando makemigrations..."
+python manage.py makemigrations
+
+# Verificar si la variable de entorno MIGRATE está configurada para ejecutar migrate
+if [ "$MIGRATE" == "true" ]; then
+  echo "Ejecutando migrate..."
+  python manage.py migrate
+else
+  echo "No se ejecutó migrate, establezca MIGRATE=true para ejecutarlo."
+fi
+
 # Ejecutar el servidor de Django
 echo "Iniciando el servidor de Django..."
 python manage.py runserver 0.0.0.0:8000
